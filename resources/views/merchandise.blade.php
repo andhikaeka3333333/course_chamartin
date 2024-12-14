2cc8e0872f52ca043b4ae458543f94cce2cb9993
Yusuf Rizqy M <yusufhabib290@gmail.com>
2024-12-14 17:24:29 +0700

    Feat[YR]: Finishing Web

--- resources/views/merchandise.blade.php
+++ resources/views/merchandise.blade.php
@@ -20,12 +20,15 @@
                 <div class="p-6">
                     <img src="images/4.png" alt="Cangkir Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">Cangkir Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp 24.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/cangkir-chamartin-merchandise-putih-e77aa?extParam=ivf%3Dfalse%26keyword%3Dcangkir+chamartin%26search_id%3D2024121310522622FDC894E756183742BS%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
                     </div>
 
 
@@ -42,12 +45,15 @@
                 <div class="p-6">
                     <img src="images/5.png" alt="Totebag Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">Totebag Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp 47.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/totebag-chamartin-merchandise-putih-6d955?extParam=ivf%3Dfalse%26keyword%3Dtotebag+chamartin%26search_id%3D2024121310543137547C10C834B92268BM%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
                     </div>
 
                 </div>
@@ -63,12 +69,15 @@
                 <div class="p-6">
                     <img src="images/6.png" alt="Bantal Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">Bantal Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp 50.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/bantal-chamartin-merchandise-putih-ceab5?extParam=ivf%3Dfalse%26keyword%3Dbantal+chamartin%26search_id%3D202412131055333CD32C33157A6733BLYL%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
                     </div>
 
                 </div>
@@ -84,12 +93,15 @@
                 <div class="p-6">
                     <img src="images/3.png" alt="Sweater Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">Sweater Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp 90.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/sweater-chamartin-merchandise-putih-780cb?extParam=ivf%3Dfalse%26keyword%3Dsweater+chamartin%26search_id%3D20241213112323B6B4BA0CC3037F2C7RQR%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
                     </div>
 
                 </div>
@@ -105,12 +117,15 @@
                 <div class="p-6">
                     <img src="images/2.png" alt="T-shirt Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">T-shirt Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp 70.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/t-shirt-chamartin-merchandise-putih-f6844?extParam=ivf%3Dfalse%26keyword%3Dtshirt+chamartin%26search_id%3D202412131124351BE6096569E69520CTJG%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
                     </div>
 
                 </div>
@@ -126,17 +141,20 @@
                 <div class="p-6">
                     <img src="images/1.png" alt="Hoodie Chamartin" class="w-full h-40 object-contain mb-4">
                     <h2 class="text-lg font-bold text-[#00d5ff] mb-2">Hoodie Chamartin</h2>
-                    <p class="text-white text-xl font-semibold mb-4">Rp.79.999</p>
+                    <p class="text-white text-xl font-semibold mb-4">Rp.90.000</p>
                     <div class="w-full">
-                        <button
-                            class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
-                            Beli sekarang
-                        </button>
+                        <a href="https://www.tokopedia.com/chamartinshop/hoodie-chamartin-merchandise-putih-d937f?extParam=ivf%3Dfalse%26keyword%3Dhoodie+chamartin%26search_id%3D20241213112618688AC34F87162F11CNZK%26src%3Dsearch"
+                            target="blank">
+                            <button
+                                class="w-full border border-white text-white bg-[#2d3e50] py-2 px-4 rounded-md hover:bg-[#2d3e5F]/90">
+                                Beli sekarang
+                            </button>
+                        </a>
+
                     </div>
 
                 </div>
             </div>
         </div>
     </section>
-
 </x-app-layout>
--- resources/views/navigation-welcome/layout.blade.php
+++ resources/views/navigation-welcome/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="images/logo.png" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
